8bbfbd5598d50051db2acad0ccdde35fb59356f6
wjk <wujikunda@126.com>
2018-02-26 20:04:59 +0800

    wechat api

--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -1,22 +1,11 @@
-import Router from 'koa-router'
-import config from '../config'
-import reply from '../wechat/reply'
-import wechatMiddle from '../wechat-lib/middleware'
-import { signature, redirect, oauth } from '../controllers/wechat'
+import { Route } from '../decorator/router'
+import { resolve } from 'path'
+
+const r = path => resolve(__dirname, path)
 
 export const router = app => {
-  const router = new Router()
+  const apiPath = r('../routes')
+  const router = new Route(app, apiPath)
 
-  router.all('/wechat-hear', wechatMiddle(config.wechat, reply))
-  router.get('/wechat-signature', signature)
-  router.get('/wechat-redirect', redirect)
-  router.get('/wechat-oauth', oauth)
-  // router.get('/upload', async (ctx, next) => {
-  //   let mp = require('../wechat')
-  //   let client = mp.getWechat()
-  //   const data = await client.handle('batchMaterial', {})
-  //   console.log(data)
-  // })
-  app.use(router.routes())
-  app.use(router.allowedMethods())
+  router.init()
 }
